227cb3fdbf4d686efe9ccba13e0ec03686bed830
Marcin <marcin.szturminski@gmail.com>
2023-06-20 21:14:08 +0200

    prototype of full page ready

--- src/js/main.js
+++ src/js/main.js
@@ -16,6 +16,90 @@ const navMobileHamburgerBox = document.querySelector('.hamburger-box')
 const allNavMobileItems = document.querySelectorAll('.nav__mobile-item')
 const bntOne = document.querySelector('.types__item')
 const modalOne = document.querySelector('.modal-one')
+const allMailInputs = document.querySelectorAll('.contact__input')
+const chooseFileLabel = document.querySelector('.contact__label-file')
+const chooseFileInput = document.querySelector('.contact__input-file')
+const contactP = document.querySelector('.contact__p')
+const allBtns = document.querySelectorAll('.contact__btn')
+
+// Check if all inputs are not empty
+
+allBtns[1].addEventListener('click', () => {
+	allMailInputs.forEach(item => {
+		switch (item.value) {
+			case '':
+				item.classList.add('show-error')
+		}
+	})
+})
+
+// Clearing input files
+
+const clearAllInputs = () => {
+	allMailInputs.forEach(item => {
+		item.value = ''
+		item.nextElementSibling.classList.remove('show-label')
+		contactP.classList.remove('show-p')
+		item.classList.remove('show-error')
+	})
+}
+
+// Showing name of file that was added - not workining!
+
+// chooseFileInput.addEventListener('change', () => {
+// 	const newFileReader = new FileReader()
+// 	newFileReader.readAsDataURL(chooseFileInput.files[0])
+// 	newFileReader.addEventListener('load', () => {
+// 		const url = newFileReader.result
+// 		console.log(url)
+// 		contactP.classList.add('show-p')
+// 		contactP.textContent = url
+// 	})
+// })
+
+// Showing that file was added
+
+chooseFileInput.onchange = function (e) {
+	this.value = null
+	contactP.classList.add('show-p')
+}
+
+// Removing label when selecting input and when input has text inside
+
+allMailInputs.forEach(item => {
+	item.addEventListener('focus', () => {
+		item.nextElementSibling.classList.add('show-label')
+		item.classList.remove('show-error')
+	})
+	item.addEventListener('focusout', () => {
+		if (item.value == '') {
+			item.nextElementSibling.classList.remove('show-label')
+		}
+	})
+})
+
+// Focus after using choose-file element
+
+const addFocusFile = () => {
+	chooseFileLabel.classList.add('show-focus')
+}
+
+const removeFocusFile = () => {
+	chooseFileLabel.classList.remove('show-focus')
+}
+
+// Removing label after focus on input element - this is working - uncoment when needed
+
+// allMailInputs.forEach(item => {
+// 	item.addEventListener('focus', () => {
+// 		item.nextElementSibling.classList.add('show-label')
+// 	})
+// 	item.addEventListener('focusout', () => {
+// 		item.nextElementSibling.classList.remove('show-label')
+// 	})
+// })
+
+// Modal for section - segregate - NOT FINISHED
 
 const showModal = () => {
 	modalOne.classList.add('show-modal')
@@ -72,3 +156,6 @@ const showMobileNavFunction = () => {
 }
 
 navMobileHamburgerBtn.addEventListener('click', showMobileNavFunction)
+chooseFileInput.addEventListener('focus', addFocusFile)
+chooseFileInput.addEventListener('focusout', removeFocusFile)
+allBtns[0].addEventListener('click', clearAllInputs)
